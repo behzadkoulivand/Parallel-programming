aaaae7354ac989c6852e29234f6d41c85ee67ec5
Behzad Koulivand <behzadk.1245@gmail.com>
2023-07-12 18:43:44 +0330

    Asynchronous Programming writed

--- app.js
+++ app.js
@@ -3,22 +3,49 @@ const express = require('express');
 const app = express();
 
 // Synchronous
-const task1 = () => {
-    let i = 10000000000;
-    let j = 0;
-    while(j < i){
-        j++;
-    }
+// const task1 = () => {
+//     let i = 10000000000;
+//     let j = 0;
+//     while(j < i){
+//         j++;
+//     }
 
-    console.log("task1 is running");
+//     console.log("task1 is running");
+// };
+
+// const task2 = () => {
+//     console.log("task2 is running");
+// };
+
+// task1();
+// task2();
+
+// Asynchronous
+const task1 = () => {
+    return new Promise((resolve, reject) => {
+        let i = 1000000000;
+        let j = 0;
+        while (j < i) {
+            j++;
+        }
+        resolve("task1 is running");
+    });
 };
 
 const task2 = () => {
-    console.log("task2 is running");
+    return new Promise((resolve, reject) => {
+        resolve("task2 is running");
+    })
+    
 };
 
-task1();
-task2();
+const tasks = [task1(), task2()];
+Promise.all(tasks)
+.then((res) =>{
+    console.log(res);
+}).catch((err)=>{
+    console.log(err);
+})
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
