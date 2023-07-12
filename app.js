8b0650dd9f0e367b1eff8f7b9079ff2e6906df5e
Behzad Koulivand <behzadk.1245@gmail.com>
2023-07-12 18:20:16 +0330

    Synchronous Programming writed

--- app.js
+++ app.js
@@ -2,6 +2,24 @@ const express = require('express');
 
 const app = express();
 
+// Synchronous
+const task1 = () => {
+    let i = 10000000000;
+    let j = 0;
+    while(j < i){
+        j++;
+    }
+
+    console.log("task1 is running");
+};
+
+const task2 = () => {
+    console.log("task2 is running");
+};
+
+task1();
+task2();
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 })
